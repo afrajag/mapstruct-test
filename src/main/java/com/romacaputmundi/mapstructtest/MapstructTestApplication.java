8627b8009f05d7c36d2f9e36752bdb908118edbe
Fabrizio Pezzola <fabrizio.pezzola@eng.it>
2021-06-26 14:38:54 +0200

    first commit

---
+++ src/main/java/com/romacaputmundi/mapstructtest/MapstructTestApplication.java
@@ -0,0 +1,13 @@
+package com.romacaputmundi.mapstructtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructTestApplication.class, args);
+	}
+
+}
